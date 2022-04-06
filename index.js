26a0002e5fe453e6def045c4d1043bad125e66e4
Souradeep Dutta <souradeepdutta2001@gmail.com>
2022-04-07 01:26:14 +0530

    deleted JS file

--- index.js
+++
@@ -1,5 +0,0 @@
-var toggler = document.querySelector(".toggler")
-var navbody = document.querySelector(".nav_body");
-toggler.addEventListener("click",function(e){
-    navbody.classList.toggle("active");
-})
